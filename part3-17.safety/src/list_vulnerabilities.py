a1fbb73ada7e7001c0a08d320f1588f8627d3253
biko2020 <aitoufkirbrahimab@gmail.com>
2023-06-04 00:16:50 +0100

    :bug:Using Components with Known Vulnerabilities

--- part3-17.safety/src/list_vulnerabilities.py
+++ part3-17.safety/src/list_vulnerabilities.py
@@ -4,15 +4,25 @@ import json
 
 
 def get_vulnerabilities(name, db):
-	return []
+	all_vulnerabilities = []
+	json_data = json.load(db)
+	for advisory  in json_data:
+
+		if (advisory == name):
+			for vulnerability in json_data[advisory]:
+				all_vulnerabilities.append(
+					(vulnerability["id"], vulnerability["v"], vulnerability["cve"] ))
+	return all_vulnerabilities
 
 
 def main(argv):
-	name = sys.argv[1]
-	db = open(sys.argv[2])
-	vulnerabilities = get_vulnerabilities(name, db)
-	for v in vulnerabilities:
-		print('%s; %s; %s' % (v[0], v[1], v[2]))
+        name = sys.argv[1]
+        db = open(sys.argv[2])
+
+        vulnerabilities = get_vulnerabilities(name, db)
+        for v in vulnerabilities:
+                print('%s; %s; %s' % (v[0], v[1], v[2]))
+
 
 
 # This makes sure the main function is not called immediatedly
